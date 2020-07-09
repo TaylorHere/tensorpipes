fd1be2b9e57a723b949015e86dda7cb5f19dd1a3
Taylorli <taylorli@lilith.com>
2020-07-09 20:38:33 +0800

    Init:

--- install-jupyterhub.sh
+++ install-jupyterhub.sh
@@ -1,9 +1,7 @@
-# Suggested values: advanced users of Kubernetes and Helm should feel
-# free to use different values.
-RELEASE=tensorpipes-jupyterhub
+RELEASE=jhub
 NAMESPACE=tensorpipes
 
 helm upgrade --install $RELEASE jupyterhub \
   --namespace $NAMESPACE  \
-  --version=0.8.2 \
+  --version=0.9.0 \
   --values config.yaml
